1dd4cf76cd96c5ffdf399d12a88497223cc74d8e
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-08-28 14:06:27 +0600

    admin and visitor role get

--- index.js
+++ index.js
@@ -91,6 +91,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users/admin/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      const user = await userCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+    app.get("/users/visitor/:email", async (req, res) => {
+      const query = { email: req.params.email };
+      const user = await userCollection.findOne(query);
+      const result = { seller: user?.role === "visitor" };
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
