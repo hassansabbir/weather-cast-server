0f95ab00210200f838075faa066ba7129a133655
JubaYer0307 <jubayerahmed0307@gmail.com>
2023-09-14 18:37:14 +0600

    community

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+
+
+
 //middleware
 app.use(cors());
 app.use(express.json());
+
+
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@webwizerd.gtwxqnt.mongodb.net/?retryWrites=true&w=majority`;
@@ -36,6 +42,7 @@ async function run() {
     const blogsCollection = client.db("weatherCast").collection("blogs");
     const articlesCollection = client.db("weatherCast").collection("articles");
     const userCollection = client.db("weatherCast").collection("users");
+    const postCollection = client.db("weatherCast").collection("post");
     const favLocationCollection = client
       .db("weatherCast")
       .collection("favLocation");
@@ -128,6 +135,235 @@ async function run() {
       const result = await articlesCollection.findOne(query);
       res.send(result);
     });
+
+
+    //PostCollection
+
+    app.get("/post", async (req, res) => {
+      const result = await postCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/post", async (req, res) => {
+      const result = await postCollection.find().toArray();
+      res.send(result);
+    });
+    
+    
+
+    app.get("/post/:email", async (req, res) => {
+      const authorEmail = req.params.email;
+      const result = await postCollection.find({ authorEmail }).toArray();
+      res.send(result);
+    });
+
+   
+  
+
+    app.post("/post", async (req, res) => {
+      const newItem = req.body;
+      newItem.comments = [];
+      
+      try {
+
+        newItem.createdAt = new Date();
+        
+        const result = await postCollection.insertOne(newItem);
+        res.json({ insertedId: result.insertedId });
+      } catch (error) {
+        console.error("Error creating post:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
+    
+
+    app.get("/post/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const post = await postCollection.findOne(query);
+    
+      if (!post.comments) {
+        post.comments = [];
+      }
+    
+      res.send(post);
+    });
+
+
+  
+
+    app.delete("/post/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+    
+      try {
+        const result = await postCollection.deleteOne(query);
+    
+        if (result.deletedCount === 1) {
+          res.json({ message: "Post deleted successfully" });
+        } else {
+          res.status(404).json({ message: "Post not found" });
+        }
+      } catch (error) {
+        console.error("Error deleting post:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    });
+
+
+   
+    
+    app.put("/post/:postId", async (req, res) => {
+      const { postId } = req.params;
+      const { content } = req.body;
+    
+      try {
+        const query = { _id: new ObjectId(postId) };
+        const update = { $set: { content } };
+    
+        const result = await postCollection.updateOne(query, update);
+    
+        if (result.modifiedCount === 1) {
+
+          const updatedPost = await postCollection.findOne(query);
+          return res.status(200).json(updatedPost);
+        } else {
+          return res.status(404).json({ message: "Post not found" });
+        }
+      } catch (error) {
+        console.error("Error updating post:", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+    });
+    
+    
+    
+
+
+    //Like
+
+app.post("/post/:id/like", async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+
+    const query = { _id: new ObjectId(postId) };
+    const update = {
+      $inc: { likes: 1 },
+      $set: { likedByCurrentUser: true },
+    };
+
+    const result = await postCollection.updateOne(query, update);
+
+    if (result.modifiedCount === 1) {
+      res.json({ message: "Post liked" });
+    } else {
+      res.status(404).json({ message: "Post not found" });
+    }
+  } catch (error) {
+    console.error("Error liking the post:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//unlike 
+app.post("/post/:id/unlike", async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+    
+    const query = { _id: new ObjectId(postId) };
+    const update = {
+      $inc: { likes: -1 },
+      $set: { likedByCurrentUser: false },
+    };
+
+    const result = await postCollection.updateOne(query, update);
+
+    if (result.modifiedCount === 1) {
+      res.json({ message: "Post unliked" });
+    } else {
+      res.status(404).json({ message: "Post not found" });
+    }
+  } catch (error) {
+    console.error("Error unliking the post:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
+
+// new comment
+app.post("/post/:id/comment", async (req, res) => {
+  const postId = req.params.id;
+  const { content, userId, userName } = req.body;
+
+  try {
+   
+    const post = await postCollection.findOne({ _id: new ObjectId(postId) });
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+    if (!post.comments) {
+      post.comments = [];
+    }
+
+    
+    const createdAt = new Date(); 
+    const newComment = {
+      _id: new ObjectId(),
+      userId,
+      userName,
+      content,
+      createdAt,
+    };
+
+    
+    post.comments.push(newComment);
+
+    
+    const result = await postCollection.updateOne(
+      { _id: new ObjectId(postId) },
+      { $set: { comments: post.comments } }
+    );
+
+    if (result.modifiedCount === 1) {
+      res.json({ message: "Comment added" });
+    } else {
+      res.status(500).json({ message: "Error adding comment" });
+    }
+  } catch (error) {
+    console.error("Error adding comment:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
+
+app.get("/post/:id/comments", async (req, res) => {
+  const postId = req.params.id;
+
+  try {
+  
+    const post = await postCollection.findOne({ _id: new ObjectId(postId) });
+
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+
+   
+    res.json({ comments: post.comments });
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+
+
+
 
     //userCollection
 
