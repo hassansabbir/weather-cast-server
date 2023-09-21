cefb6018b728f93c3c4d565acd9a2e75288b91b0
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-09-21 18:55:59 +0600

    updated

--- index.js
+++ index.js
@@ -688,7 +688,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("weather is forecasting...");
+  res.send("weatherCast is forecasting...");
 });
 
 app.listen(port, () => {
